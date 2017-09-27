b7d01830d55cad8fbe5e62dc4a5effaf408dd39c
刘伯君 <liubj@wangsu.com>
2017-09-27 11:17:24 +0800

    未完成的提交

--- src/service/getData.js
+++ src/service/getData.js
@@ -8,3 +8,7 @@ const baseUrl = 'http://www.easy-mock.com/mock/59c76e49e0dc663341b717dc/example'
 
 export const hotCity = () => fetch(baseUrl + '/hotcity');
 export const groupCity = () => fetch(baseUrl + '/groupcity');
+
+export const msiteAddress = () => fetch(baseUrl + '/pois').then(res => res.json());
+export const cityGuess = () => fetch(baseUrl + '/cityGuess').then(res => res.json());
+export const msiteFoodTypes = () => fetch(baseUrl + '/foodTypes').then(res => res.json());
